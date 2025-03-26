2186e1fc65c892a5bef5046aa34e60e93376eec3
aldairjunior33 <aldairjunior33@gmail.com>
2025-03-26 16:02:16 -0300

    feat(transformacao_dados): extrai, transforma e compacta dados do Anexo I em CSV
    Transforma os dados extraídos em um único DataFrame, substitui as abreviações 'OD' e 'AMB' por
    suas descrições completas ('Odontologia' e 'Ambulatorial'), e salva o resultado em um arquivo CSV.
    O CSV é compactado em um arquivo .zip.
    Arquivos de saída são salvos no diretório 'transformacao_dados/saida

--- transformacao_dados/anexo1_transformacao.py
+++ transformacao_dados/anexo1_transformacao.py
@@ -1,17 +1,31 @@
 import pdfplumber
 import os
 import pandas as pd
+import zipfile
 
 pdf_path = os.path.join('.', 'anexos', 'Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf')
 
-with pdfplumber.open(pdf_path) as pdf:
-    for i, page in enumerate(pdf.pages):
-        print(f"\n + Página {i+1}")
+tables = []
 
+with pdfplumber.open(pdf_path) as pdf:
+    for page in pdf.pages:
         table = page.extract_table()
 
         if table:
-            df = pd.DataFrame(table[1:], columns=table[0])  # Ignora a primeira linha se for cabeçalho duplicado
-            print(df.head())  # Mostra apenas as primeiras linhas da tabela
-        else:
-            print("⚠️ Nenhuma tabela encontrada nesta página.")
+            df_page = pd.DataFrame(table[1:], columns=table[0])  # Ignora a primeira linha se for cabeçalho duplicado
+            tables.append(df_page)
+
+# juntar todas as tabelas em um unico lugar
+df_total = pd.concat(tables, ignore_index=True)
+# Replace é um metodo panda que altera valores definidos dentro das tabelas.
+df_total.replace({'OD': 'Odontologia', 'AMB': 'Ambulatorial'}, inplace=True)
+
+os.makedirs('transformacao_dados/saida', exist_ok=True)
+csv_path = 'transformacao_dados/saida/Teste_AldairJoseTenorioMoreiraJunior.csv'
+df_total.to_csv(csv_path, index=False)
+
+zip_path = 'transformacao_dados/saida/Teste_AldairJoseTenorioMoreiraJunior.zip'
+with zipfile.ZipFile(zip_path, 'w') as zipf:
+    zipf.write(csv_path, arcname=os.path.basename(csv_path))
+
+print(f"Transformação finalizada. Arquivo salvo e compactado em: {zip_path}")
